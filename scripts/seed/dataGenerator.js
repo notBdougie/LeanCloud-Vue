b053a138a5b792670942ec620fe96a768466f4a1
Edward Chu <crazyzyt@gmail.com>
2016-05-10 22:08:06 +0800

    add roles seeding

--- scripts/seed/dataGenerator.js
+++ scripts/seed/dataGenerator.js
@@ -12,12 +12,82 @@ AV.Cloud.useMasterKey()
 // load data
 const data = yaml.safeLoad(fs.readFileSync(__dirname + '/data.yml', 'utf8'))
 
+/**
+roles:
+  - name: Editor
+  - name: Admin
+    users:
+      - chuyik
+    roles:
+      - Editor
+ */
+export function setRoles () {
+  let roles = data.roles
+  return AV.Promise.all(roles.map(function (_role) {
+    
+    let query = new AV.Query(AV.Role)
+    query.equalTo('name', _role.name)
+    return query.first()
+      .then((result) => {
+        // find and save role
+        if (!result) {
+          // let roleACL = new AV.ACL()
+          // roleACL.setPublicReadAccess(true)
+          let role = new AV.Role(_role.name)
+          return role.save()
+        } else {
+          console.log('found role: ', result.get('name'))
+          return nextThen(result)
+        }
+      })
+      .then((role) => {
+        let users = _role.users
+        if (!users) {
+          return nextThen(role)
+        }
+        // find users
+        let userQuery = new AV.Query(AV.User)
+        userQuery.containedIn('username', users)
+        
+        return userQuery.find(users).then((users) => {
+          role.getUsers().add(users)
+          return role.save()
+        })
+      })
+  }))
+  .then((results) => {
+    // add roles relations
+    console.log('roles: ', results.length)
+    let promises = []
+    roles.forEach((roleData) => {
+      if (roleData.roles) {
+        // find corresponding role object
+        let roleObject = _.find(results, function (result) {
+          return result.get('name') === roleData.name 
+        })
+        if (roleObject) {
+          // find role objects to set
+          let roleObjects = _.filter(results, (result) => {
+            return _.includes(roleData.roles, result.get('name'))
+          })
+          console.log(`${roleObject.get('name')} has ${roleObjects.length} sub roles`)
+          roleObject.getRoles().add(roleObjects)
+          promises.push(roleObject.save())
+        }
+      }
+    })
+    return AV.Promise.all(promises)
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+}
 
 export function addUsers () {
   let users = data.user
     
   return AV.Promise.all(users.map(function (_user) {
-    var user = _user
+    let user = _user
     user.password = user.password || user.username
     // set avatar
     const file = AV.File.withURL(`avatar_${user.username}.jpg`, user.avatar || getAvatarUrl(user.username))
@@ -36,16 +106,16 @@ export function addUsers () {
 export function followUsers () {
   let followRelations = data.follow 
   return new AV.Query(AV.User).find((users) => {
-    var promises = []
+    let promises = []
     for (let relation of followRelations) {
-      var followee = _.find(users, function (user) {
+      let followee = _.find(users, function (user) {
         return user.get('username') === relation.username
       })
       if (!followee) {
         console.error(`cannot find followee: ${relation.username}`)
         continue
       }
-      var followers = _.filter(users, (user) => {
+      let followers = _.filter(users, (user) => {
         return _.find(relation.followers, {'username': user.get('username')})
       })
       
@@ -64,8 +134,8 @@ export function followUsers () {
 export function addChannels () {
   let channels = data.channel
   return AV.Promise.all(channels.map(function (_channel) { 
-    var Channel = AV.Object.extend('AURChannel')
-    var channel = new Channel()
+    let Channel = AV.Object.extend('AURChannel')
+    let channel = new Channel()
     return channel.save(_channel)
   }))
 }
@@ -73,7 +143,7 @@ export function addChannels () {
 export function addNews () {
   let newsList = data.news
   let News = AV.Object.extend('AURNews')
-  var users, channels
+  let users, channels
   return AV.Promise.all([new AV.Query(AV.User).find(), new AV.Query('AURChannel').find()])
     .then((values) => {
       users = values[0]
@@ -102,8 +172,8 @@ export function addNews () {
 }
 
 export function testConnection () {
-  var TestObject = AV.Object.extend('TestObject')
-  var testObject = new TestObject()
+  let TestObject = AV.Object.extend('TestObject')
+  let testObject = new TestObject()
   testObject.save({
     testabc: 'abc123'
   }).then(function() {
@@ -120,7 +190,7 @@ function getAvatarUrl (key) {
 }
 
 function register (userData) {
-  var user = new AV.User()
+  let user = new AV.User()
   user.set(userData)
   return user.signUp()
 }
@@ -134,21 +204,21 @@ function follow (followee) {
 }
 
 function getUser (username) {
-    var query = new AV.Query(AV.User)
+    let query = new AV.Query(AV.User)
     query.equalTo('username', username)
     return query.first()
 }
 
 function sleep (time) {
-    var promise = new AV.Promise()
+    let promise = new AV.Promise()
         setTimeout(function() {
             promise.resolve()
         }, time)
     return promise
 }
 
-function nextThen () {
-    var promise = new AV.Promise()
-    promise.resolve()
+function nextThen (value) {
+    let promise = new AV.Promise()
+    promise.resolve(value)
     return promise
 }
