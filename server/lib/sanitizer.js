0a4481fb3d384f0431ea33cedff747ad52f9795c
Edward Chu <crazyzyt@gmail.com>
2016-05-23 10:52:14 +0800

    add comments on sanitizer

--- server/lib/sanitizer.js
+++ server/lib/sanitizer.js
@@ -1,22 +1,43 @@
 const _ = require('lodash')
 
+const ReservedKeys = [
+  '__v', '_id' // MongooseReservedKeys
+]
+const Methods = ['POST','DELETE','PUT']
+
+/**
+ * Sanitize the data of req.body
+ */
 exports.DataSanitizer = function(req, res, next) {
   
-  if (!req.body || ['POST','DELETE','PUT'].indexOf(req.method) === -1) 
+  // skipped if request data is empty
+  // or not specified request methods
+  if (!req.body || Methods.indexOf(req.method) === -1) 
     return next()
 
+  // only inspect outermost level
+  let sanitizedData
   if (_.isArray(req.body)) {
-    req.body = req.body.map(omitValuesByKeys)
+    sanitizedData = req.body.map(function (object) {
+      return omitNonPersistentData(object, ReservedKeys)
+    })
   } else {
-    req.body = omitValuesByKeys(req.body)
+    sanitizedData = omitNonPersistentData(req.body, ReservedKeys)
   }
+  req.body = sanitizedData
   return next()
 }
 
-function omitValuesByKeys (object) {
+/**
+ * Omit object keys which start with underline,
+ * but not including the keys passed in
+ */
+function omitNonPersistentData (object, ignoredKeys=[]) {
   if (!_.isPlainObject(object)) return object
     
   return _.omitBy(object, function (value, key) {
-    return ['__v', '_id'].indexOf(key) === -1 && _.startsWith(key, '_')
+    return ignoredKeys.indexOf(key) === -1 && _.startsWith(key, '_')
   })
 }
+
+exports.omitNonPersistentData = omitNonPersistentData
