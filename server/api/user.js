823c0dbea635c25cb9bc655dd54c41f50831f97a
Edward Chu <crazyzyt@gmail.com>
2016-06-28 20:42:02 +0800

    return more useful current user data

--- server/api/user.js
+++ server/api/user.js
@@ -19,11 +19,37 @@ exports.logout = function(req, res) {
   res.end()
 }
 
-exports.me = function(req, res) {
+/**
+ * Return current user data.
+ * But these fields are also included:
+ *  _roles
+ *  _sessionToken
+ */
+exports.me = function(req, res, next) {
   let sessionToken = req.AV.user._sessionToken
-  let user = req.AV.user.toJSON()
-  user._sessionToken = sessionToken
-  res.json(user)
+  let user = req.AV.user
+
+  // Convert AVUser to plain JSON object
+  let result = user.toJSON()
+  result._sessionToken = sessionToken
+
+  // Query the user's roles
+  let query = new AV.Query(AV.Role)
+  query.equalTo('users', user)
+
+  query.find()
+    .then(roles => {
+      if (!roles) {
+        result._roles = []
+      } else {
+        result._roles = roles.map(role => {
+          return role.get('name')
+        })
+      }
+
+      res.json(result)
+    })
+    .catch(next)
 }
 
 module.exports = exports
