b6d0e1a6c29217c2f3924c49eab65c9b0fb8732c
Edward Chu <crazyzyt@gmail.com>
2016-07-04 14:18:52 +0800

    add client side router interceptor

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import UserProvider from '../lib/UserProvider'
 
 let root = process.env.serverConfig.portalPrefix
 
@@ -27,6 +28,36 @@ router.map({
     '*': {
         component: require('../components/NotFound')
     }
+})
+
+// Redirect to login page if route is not public and user not logged in
+router.beforeEach(function ({to, next}) {
+    if (to.public)
+        return next()
+    
+    let user = UserProvider.getCurrentUser()
+    if (user) {
+        // user is logged in
+        return next()
+    } else {
+        // try to load from server
+        return new Promise(resolve => {
+            UserProvider.loadFromServer()
+                .then(() => {
+                    resolve(!!UserProvider.getCurrentUser())
+                })
+                .catch(() => {
+                    resolve(false)
+                })
+        })
+    }
+})
+
+// Broadcast an event on route changed 
+router.afterEach(function ({to}) {
+    let vm = to.router.app
+    vm.$dispatch('onRouteChanged', to)
+    vm.$broadcast('onRouteChanged', to)
 })
 
 export default router
