5f2370b4d66b7c4497281ba7bc18f31d48cb113f
Edward Chu <crazyzyt@gmail.com>
2016-07-05 19:11:35 +0800

    enhance portal prefix support

--- builder/dev-client.js
+++
@@ -1,7 +0,0 @@
-const hotClient = require('webpack-hot-middleware/client?path=__webpack_hmr&noInfo=true&reload=true')
-
-hotClient.subscribe(function (event) {
-  if (event.action === 'reload') {
-    window.location.reload()
-  }
-})
--- builder/webpack.dev.config.js
+++ builder/webpack.dev.config.js
@@ -2,10 +2,14 @@ const webpack = require('webpack')
 const merge = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.config')
+const config = require('./config')
+
+const {portalPrefix} = config.serverConfig
+const hotMiddleware = `webpack-hot-middleware/client?path=${portalPrefix}__webpack_hmr&noInfo=true&reload=true`
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
-  baseWebpackConfig.entry[name] = ['./builder/dev-client'].concat(baseWebpackConfig.entry[name])
+  baseWebpackConfig.entry[name] = [hotMiddleware].concat(baseWebpackConfig.entry[name])
 })
 
 module.exports = merge(baseWebpackConfig, {
